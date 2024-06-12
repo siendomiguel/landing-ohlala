bd5305d9abcb0c8e98e384e9bcaca2d698396fa8
Miguel Lares <siendomigueldev@gmail.com>
2024-06-12 09:51:27 -0500

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 
 import './globals.css';
 
@@ -15,7 +16,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <GoogleTagManager gtmId="G-2VZJJ6N2E5" />
+      {/* <GoogleTagManager gtmId="G-2VZJJ6N2E5" /> */}
+
+      <Script
+        id="gtm"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WDTVXHCM');`,
+        }}
+      ></Script>
+
       <body className="min-h-screen flex flex-col bg-gradient-to-b from-purple-100 to-white">
         <div className="flex-grow flex flex-col">{children}</div>
         <footer className="text-center py-4 w-full bg-purple-700 text-white rounded-t-lg">
